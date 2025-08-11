68316c918c522b1d700bab9e38bd80fe9fed0135
kmjyz44 <83801081+kmjyz44@users.noreply.github.com>
2025-08-10 21:17:07 -0500

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // якщо використовуєш App Router — все ок
+  // додатково можна вмикати експерименти за потреби:
+  // experimental: { esmExternals: 'loose' },
+};
+
+export default nextConfig;
